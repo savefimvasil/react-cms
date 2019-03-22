429d4e8a24aaa992a601a3f6df739e190a51e4ad
savefimvasil <savefimvasil@gmail.com>
2019-03-22 23:30:21 +0200

    clear offer page

--- components/ListOfOffers/ListOfOffers.js
+++ components/ListOfOffers/ListOfOffers.js
@@ -3,39 +3,6 @@ import './ListOfOffers.scss'
 import CardItem from '../CardItem/CardItem'
 
 class ListOfOffers extends Component {
-    state = {
-        list: [
-            {
-                _id: 1,
-                titleUA: 'post1',
-                date: '27',
-                img: 'https://s-ec.bstatic.com/images/hotel/max1024x768/801/80176477.jpg',
-                description: 'post1'
-            },
-            {
-                _id: 2,
-                titleUA: 'post2',
-                date: '28',
-                img: 'https://s-ec.bstatic.com/images/hotel/max1024x768/801/80176477.jpg',
-                description: 'post2'
-            },
-            {
-                _id: 3,
-                titleUA: 'post3',
-                date: '29',
-                img: 'https://s-ec.bstatic.com/images/hotel/max1024x768/801/80176477.jpg',
-                description: 'post3'
-            },
-            {
-                _id: 4,
-                titleUA: 'post4',
-                date: '30',
-                img: 'https://s-ec.bstatic.com/images/hotel/max1024x768/801/80176477.jpg',
-                description: 'post4'
-            }
-        ]
-    }
-
     render() {
         return (
             <div className={'List container'}>
